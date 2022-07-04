cfad01ded723ce649f87e41ac98f7f0a7ced9e9a
Sadullah TANRIKULU <sadullahtanrikulu@gmail.com>
2022-07-04 17:43:05 +0300

    project finished

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,6 @@ import ShowCloseButton from "./ShowCloseButton";
 const Header = (props) => {
     return ( 
         <div className="header flex flex-col justify-center items-center gap-4 w-80 pt-4">
-            
             <ShowCloseButton props = {props}/>
 
         </div>
